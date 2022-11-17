367ec6989a20b2783eb2e848fc74890250ae3d2f
multiflexer <_>
2022-11-17 03:01:53 +0300

    Обработал возможную ошибку.

--- src/api/v1/admin/permissions.py
+++ src/api/v1/admin/permissions.py
@@ -4,14 +4,12 @@ from flask import request, Blueprint, jsonify
 
 from services.user_service import get_user_service
 from services.db_service import get_db_service
-from utils.auth import admin_token_required
 
 
 admin_permissions_blueprint_v1 = Blueprint("admin_permissions_blueprint_v1", __name__, url_prefix="/admin/api/v1")
 
 
 @admin_permissions_blueprint_v1.route("/check_permission", methods=["GET"])
-@admin_token_required
 def has_permission(*args, **kwargs):
     """
     Проверка наличия прав у пользователя на ресурс.
@@ -38,6 +36,6 @@ def has_permission(*args, **kwargs):
     user_dto = user_service.get_user_by_user_id(user_id)
     resource_role = db_service.get_role(resource_role_name)
     user_role = db_service.get_role(user_dto.role)
-    if user_role.position >= resource_role.position:
+    if user_role and user_role.position >= resource_role.position:
         return jsonify({"result": "success"}), HTTPStatus.OK
     return jsonify({"result": "error"}), HTTPStatus.FORBIDDEN
